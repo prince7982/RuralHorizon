5da96eb7e1ed8d18d1818e7a4b55972252d659fd
prince7982 <princekumar13810@gmail.com>
2026-04-13 19:55:55 +0530

    api fix

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -5,7 +5,7 @@ exports.handler = async function (event) {
     const { message } = JSON.parse(event.body);
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
